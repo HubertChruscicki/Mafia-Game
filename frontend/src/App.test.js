b1a2e959317541cb0db0c716230f87fac0b5e4b5
balicz3k <balicz3k@gmail.com>
2026-05-05 10:18:00 +0200

    init(frontend): bootstrap React app structure and base styling

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders application start title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByRole('heading', { name: /eMafia/i });
+  expect(titleElement).toBeInTheDocument();
 });
