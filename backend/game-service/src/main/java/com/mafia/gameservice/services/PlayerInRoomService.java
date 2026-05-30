6a5d6df31e8502adcd76c49f5e3679bd83ef7414
Marcin Fortuna <marcin.fortuna@onet.eu>
2026-05-30 10:34:22 +0200

    feat(game-service): add membership service for join and leave actions

---
+++ backend/game-service/src/main/java/com/mafia/gameservice/services/PlayerInRoomService.java
@@ -0,0 +1,21 @@
+package com.mafia.gameservice.services;
+
+import com.mafia.gameservice.models.GameRoom;
+import com.mafia.gameservice.models.PlayerInRoom;
+import com.mafia.gameservice.models.User;
+import com.mafia.gameservice.repositories.PlayerInRoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PlayerInRoomService {
+  private final PlayerInRoomRepository playerInRoomRepository;
+
+  public void addPlayerToGameRoom(User user, GameRoom room) {
+    var playerInRoom = new PlayerInRoom();
+    playerInRoom.setUser(user);
+    playerInRoom.setGameRoom(room);
+    playerInRoomRepository.save(playerInRoom);
+  }
+}
